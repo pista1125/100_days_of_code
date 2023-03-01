b478756720bbe7f9804857e24f42657569cdcfd3
pista1125 <pista1125@gmail.com>
2023-03-01 19:49:44 +0100

    day9

--- day9/main.py
+++ day9/main.py
@@ -112,37 +112,69 @@
 # important code from quiz
 # print(travel_log[0]["cities"][2])
 
-#6. Final game
+#6. Final game my version
+# import art
+# print(art.logo)
+# game = []
+# def person(name, score):
+#     extend = {}
+#     extend["name"] = name
+#     extend["score"] = score
+#     game.append(extend)
+#
+# eper_final = []
+# def calculator(eper_final):
+#     eper = []
+#     for score in game:
+#         a = score["score"]
+#         eper.append(a)
+#     eper_final += eper
+#     b = (max(eper_final))
+#     position = eper_final.index(b)
+#     print(f"Winner is {game[position]['name']} because she/he score was {game[position]['score']}.")
+# game_end = True
+# while game_end:
+#     n = input("what is your name?\n")
+#     s = int(input("What is your score?\n"))
+#     person(n, s)
+#     h = input("do you want continue? type yes, or no\n")
+#     if h == 'no':
+#         game_end = False
+#         calculator(eper_final)
+#     elif h == "yes":
+#         game_end = True
+#     else:
+#         game_end = False
+#         print("You are stupid")
+#         calculator(eper_final)
 
-game = []
-def person(name, score):
-    extend = {}
-    extend["name"] = name
-    extend["score"] = score
-    game.append(extend)
 
-eper_final = []
-def calculator(eper_final):
-    eper = []
-    for score in game:
-        a = score["score"]
-        eper.append(a)
-    eper_final += eper
-    b = (max(eper_final))
-    position = eper_final.index(b)
-    print(f"Winner is {game[position]['name']} because she/he score was {game[position]['score']}.")
-game_end = True
-while game_end:
-    n = input("what is your name?\n")
-    s = int(input("What is your score?\n"))
-    person(n, s)
-    h = input("do you want continue? type yes, or no\n")
-    if h == 'no':
-        game_end = False
-    elif h == "yes":
-        game_end = True
-    else:
-        game_end = False
-        print("You are stupid")
 
-calculator(eper_final)
+#7. Final game origin version
+
+# from art import logo
+#
+# print(logo)
+#
+# bids = {}
+# bidding_finished = False
+#
+# def find_highest_bidder(bidding_record):
+#     highest_bid = 0
+#     winner = ""
+#     # bidding_record = {"Angela": 123, "James": 321}
+#     for bidder in bidding_record:
+#         bid_amount = bidding_record[bidder]
+#         if bid_amount > highest_bid:
+#             highest_bid = bid_amount
+#             winner = bidder
+#     print(f"The winner is {winner} with a bid of ${highest_bid}")
+#
+# while not bidding_finished:
+#     name = input("What is your name?: ")
+#     price = int(input("What is your bid?: $"))
+#     bids[name] = price
+#     should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+#     if should_continue == "no":
+#         bidding_finished = True
+#         find_highest_bidder(bids)

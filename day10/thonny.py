0c8863dc50f44c02462cab2bec2076a561b0ef78
pista1125 <pista1125@gmail.com>
2023-03-27 14:55:01 +0200

    commit

--- day10/thonny.py
+++ day10/thonny.py
@@ -1,17 +1,31 @@
 from turtle import Turtle, Screen
 import random
-timmy = Turtle()
-timmy.shape("turtle")
 
-list = ["red", "blue", "silver", "medium slate blue"]
+my_sreen = Screen()
+guess = my_sreen.textinput(title="Guess the winner", prompt="Who will win?")
 
-for x in range(3):
-    color = random.choice(list)
-    timmy.color(color)
-    a = int(360 / (x+3))
-    for y in range(a):
-        timmy.right(a)
-        timmy.forward(100)
+color = ["red", "blue", "green", "yellow", "orange"]
+y_position = [-200, -100, 0, 100, 200]
+all_turtle = []
 
-my_screen = Screen()
-my_screen.exitonclick()
+for timmy_index in range(0, 5):
+    timmy = Turtle(shape="turtle")
+    timmy.color(color[timmy_index])
+    timmy.penup()
+    timmy.goto(x=-200, y=y_position[timmy_index])
+    all_turtle.append(timmy)
+
+game_on = True
+while game_on:
+    for turtle in all_turtle:
+        if turtle.xcor() > 290:
+            game_on = False
+            winning = turtle.pencolor()
+            if winning == guess:
+                print(f"You have won! The {winning} turtle is the winner!")
+            else:
+                print(f"You have lost! The {winning} turtle is the winner!")
+        rand_distance = random.randint(-2, 10)
+        turtle.forward(rand_distance)
+
+my_sreen.exitonclick()

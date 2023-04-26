672d10343e3939a7289ab5ce5ea65436b4ee5ed5
pista1125 <pista1125@gmail.com>
2023-04-26 09:08:03 +0200

    commit

--- day25/main.py
+++ day25/main.py
@@ -117,4 +117,5 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
+print("hello")
 

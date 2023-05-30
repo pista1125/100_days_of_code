4837ad8d8f58a3fbdd964ea641e31fe4c1fc1356
pista1125 <pista1125@gmail.com>
2023-05-30 14:29:45 +0200

    commit

--- day28/main.py
+++ day28/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import time
+import math
 
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,11 +9,59 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-def start():
-    timer.config(text="Work")
-    for x in range(25, 0, -1):
-                
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_label.config(text="")
+    global reps
+    reps = 0
+
+
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        timer_label.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count -1)
+
+    else:
+        start_timer()
+        marks = ""
+        work_session = math.floor(reps / 2)
+        for _ in range(work_session):
+            marks += "✔"
+        check_label.config(text=marks)
+
+
 
 window = Tk()
 window.title("Pomodoro")
@@ -23,20 +71,23 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
 #Label
 
-timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW, highlightthickness=0)
-timer.grid(row=0, column=1)
+timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW, highlightthickness=0)
+timer_label.grid(row=0, column=1)
 
-start_button = Button(text="Start", font=(FONT_NAME, 12), command=start)
+start_button = Button(text="Start", font=(FONT_NAME, 12), command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 12))
+reset_button = Button(text="Reset", font=(FONT_NAME, 12), command=reset_timer)
 reset_button.grid(row=2, column=2)
 
-check_label = Label(text="✔", bg=YELLOW, fg=GREEN, highlightthickness=0)
+check_label = Label(text="", bg=YELLOW, fg=GREEN, highlightthickness=0)
 check_label.grid(row=3, column=1)
+
+
 window.mainloop()

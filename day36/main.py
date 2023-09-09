1d5d3747536baf9163326e7c480b6f8b8abf2749
pista1125 <pista1125@gmail.com>
2023-09-09 21:48:54 +0200

    commit

--- day36/main.py
+++ day36/main.py
@@ -1,7 +1,11 @@
 import requests
+from twilio.rest import Client
 
-STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+
+STOCK_NAME = "XRP"
+COMPANY_NAME = "RIPPLE"
+
+kripto_url = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 #api key for alphavantage
@@ -10,53 +14,60 @@ api_key = "LRUBG2QIUU9JNDFF"
 #api key for newsapi
 api_key_news = "cd57936e1ce34f338ebbf5dfa354cfbf"
 
+#api key for sms sending
+account_sid = 'ACc9759d9b6afb3e259a72127956af0c4a'
+auth_token = '6d24935aac1ef8f19ea13e5ba9cf53f5'
+
+def sending_sms(text):
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_='+14327772526',
+        body=text,
+        to='+36307270793'
+    )
+
 #1. Step: Api call
-tesla_url = "https://www.alphavantage.co/query"
-tesla_param = {
-    "function":"TIME_SERIES_Daily",
+kripto_param = {
+    "function":"DIGITAL_CURRENCY_DAILY",
     "symbol":STOCK_NAME,
+    "market": "EUR",
     "apikey": api_key
 }
 
-response = requests.get(url=tesla_url, params=tesla_param)
+response = requests.get(url=kripto_url, params=kripto_param)
 response.raise_for_status()
-tesla_data = response.json()["Time Series (Daily)"]
-data_list = [value for (key, value) in tesla_data.items()]
+kripto_data= response.json()['Time Series (Digital Currency Daily)']
+
+data_list = [value for (key, value) in kripto_data.items()]
 
 yesterday_data = data_list[0]
-yesterday_closing_price = float(yesterday_data["4. close"])
+yesterday_closing_price = float(yesterday_data["4a. close (EUR)"])
 
 before_yesterday_data = data_list[1]
-before_yesterday_closing_price = float(before_yesterday_data["4. close"])
+before_yesterday_closing_price = float(before_yesterday_data["4a. close (EUR)"])
 
-difference = abs(yesterday_closing_price - before_yesterday_closing_price)
+difference = yesterday_closing_price - before_yesterday_closing_price
+diff_percent = round((difference / yesterday_closing_price) * 100, 2)
 
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+#2. checking that need search news:
+if abs(diff_percent) > 5:
+    pass
+news_params = {
+    "apiKey": api_key_news,
+    "qInTitle": COMPANY_NAME,
+}
+response_news= requests.get(NEWS_ENDPOINT, params=news_params)
+response_news.raise_for_status()
+articles = response_news.json()["articles"]
+three_articles = articles[:3]
 
-diff_percent = (difference / yesterday_closing_price) * 100
-if diff_percent >0.2:
-    news_params = {
-        "apiKey": api_key_news,
-        "qInTitle": COMPANY_NAME,
-    }
-    response_news= requests.get(NEWS_ENDPOINT, params=news_params)
-    response_news.raise_for_status()
-    articles = response_news.json()["articles"]
-    three_articles = articles[:3]
-    print(three_articles)
-## STEP 2: Use
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+#3. Sending sms from text
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
-
-
-#Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+sms_text = [f" Stock name: {STOCK_NAME}: {up_down} {diff_percent}Headline: {article['title']} \nBrief: {article['description']} \n" for article in three_articles]
+for x in range(3):
+    sending_sms(sms_text[x])

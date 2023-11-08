de6360dc1bd66320959f8f382192b1ea6585c2ef
pista1125 <pista1125@gmail.com>
2023-11-08 10:54:12 +0100

    commit

--- day38/main.py
+++ day38/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 Gender = "male"
 Weight = 100
@@ -11,7 +12,6 @@ API_KEY = "059db4892b720940df8b1bd346d5a2b3"
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 exercise_text = input("Tell me which exercises you did: ")
-
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY
@@ -27,4 +27,28 @@ parameters = {
 
 response = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
-print(result)
+
+#add a time to code:
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+
+#Google sheet add
+sheety_endpoint = "https://api.sheety.co/3727bd71eca068dbb436d2a3d48374eb/myWorkouts/workouts"
+
+retrieve_response = requests.get(url=sheety_endpoint)
+row = retrieve_response.json()
+
+#2 take information to google sheet
+
+for data in result["exercises"]:
+    new_data = {
+        "workout": {
+            "date" : today_date,
+            "time": now_time,
+            "exercise": data["user_input"],
+            "duration": data["duration_min"],
+            "calories": data["nf_calories"]
+            }
+    }
+    add_response = requests.post(url=sheety_endpoint, json=new_data)
